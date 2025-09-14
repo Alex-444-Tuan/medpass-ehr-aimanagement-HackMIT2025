54350a5ef026e8c97639e99bf4ab2e6954f4eff9
Alex-444-Tuan <anhtuanteo2006@gmail.com>
2025-09-14 02:56:50 +0000

    change port for better connection with VM

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // listen on all interfaces
+    port: 3000,        // your desired port
+    strictPort: true
+  }
 })
